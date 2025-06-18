3b9b43f825eeb05be517fdd551779c5d6a5fe3ff
JulianoSaroba123 <julianosaroba19.jsp@gmail.com>
2025-06-18 17:13:39 -0300

    Update cliente_model.py

--- models/cliente_model.py
+++ models/cliente_model.py
@@ -1,15 +1,14 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
+from models import db
 
 class Cliente(db.Model):
-    __tablename__ = 'clientes'
-
     id = db.Column(db.Integer, primary_key=True)
-    codigo = db.Column(db.String(20))  # Novo campo: código do cliente
-    nome = db.Column(db.String(100), nullable=False)
-    cpf_cnpj = db.Column(db.String(18), nullable=False)
+    codigo = db.Column(db.String(20))
+    nome = db.Column(db.String(100))
+    cpf_cnpj = db.Column(db.String(20))
     telefone = db.Column(db.String(20))
     email = db.Column(db.String(100))
-    endereco = db.Column(db.String(200))
-    numero = db.Column(db.String(10))  # Novo campo: número da residência
+    endereco = db.Column(db.String(100))
+    numero = db.Column(db.String(10))
+
+    def __repr__(self):
+        return f'<Cliente {self.nome}>'
