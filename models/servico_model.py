21f763da8ce749390553143c831102c54deedc5f
JulianoSaroba123 <julianosaroba19.jsp@gmail.com>
2025-06-19 00:21:33 -0300

    Update servico_model.py

--- models/servico_model.py
+++ models/servico_model.py
@@ -1,16 +1,16 @@
-# models/servico_model.py
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 from models import db
 
-
 class Servico(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    codigo = db.Column(db.String(50), nullable=False)
+    codigo = db.Column(db.String(20), nullable=False)
     nome = db.Column(db.String(100), nullable=False)
-    descricao = db.Column(db.String(200))
     valor = db.Column(db.Float, nullable=False)
-    unidade = db.Column(db.String(20))
-    situacao = db.Column(db.String(20))  # Ativo / Inativo
+    unidade = db.Column(db.String(10), nullable=False)
+    situacao = db.Column(db.String(20), default='Ativo')  # Ativo/Inativo
+    data_cadastro = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return f"<Servico {self.nome}>"
