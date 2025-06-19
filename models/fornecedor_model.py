6de0e5a31f1d2f51c0921f89d59f535ec8ae7780
JulianoSaroba123 <julianosaroba19.jsp@gmail.com>
2025-06-19 14:38:43 -0300

    Update fornecedor_model.py

--- models/fornecedor_model.py
+++ models/fornecedor_model.py
@@ -1,1 +1,15 @@
+from flask_sqlalchemy import SQLAlchemy
+from models import db
 
+class Fornecedor(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    codigo = db.Column(db.String(20), nullable=False)
+    nome = db.Column(db.String(100), nullable=False)
+    cnpj = db.Column(db.String(20))
+    telefone = db.Column(db.String(20))
+    email = db.Column(db.String(100))
+    endereco = db.Column(db.String(100))
+    numero = db.Column(db.String(10))
+
+    def __repr__(self):
+        return f"<Fornecedor {self.nome}>"
