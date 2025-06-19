a2a586c69c528f29d624065a4deb1a8c89ca31ef
JulianoSaroba123 <julianosaroba19.jsp@gmail.com>
2025-06-19 16:52:32 -0300

    Update fornecedor_model.py

--- models/fornecedor_model.py
+++ models/fornecedor_model.py
@@ -1,12 +1,12 @@
-
 from models import db
 
 class Fornecedor(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    codigo = db.Column(db.String(20), nullable=False, unique=True)  # <-- Adicione essa linha
     nome = db.Column(db.String(100), nullable=False)
-    cnpj = db.Column(db.String(18), nullable=False)       # Com formatação XX.XXX.XXX/0001-XX
-    telefone = db.Column(db.String(15), nullable=False)   # Com formatação (XX) XXXXX-XXXX
-    cep = db.Column(db.String(9), nullable=False)         # Formato XXXXX-XXX
+    cnpj = db.Column(db.String(18), nullable=False)
+    telefone = db.Column(db.String(15), nullable=False)
+    cep = db.Column(db.String(9), nullable=False)
     cidade = db.Column(db.String(50), nullable=False)
     endereco = db.Column(db.String(150), nullable=False)
 
