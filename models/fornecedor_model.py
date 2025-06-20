bd4b17d6402dd109ef0cbb31aeeee7d359db11e6
JulianoSaroba123 <julianosaroba19.jsp@gmail.com>
2025-06-20 15:07:52 -0300

    Update fornecedor_model.py

--- models/fornecedor_model.py
+++ models/fornecedor_model.py
@@ -2,13 +2,13 @@ from models import db
 
 class Fornecedor(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    codigo = db.Column(db.String(20), nullable=False, unique=True)  # <-- Adicione essa linha
+    codigo = db.Column(db.String(20), unique=True)
     nome = db.Column(db.String(100), nullable=False)
     cnpj = db.Column(db.String(18), nullable=False)
-    telefone = db.Column(db.String(15), nullable=False)
-    cep = db.Column(db.String(9), nullable=False)
-    cidade = db.Column(db.String(50), nullable=False)
-    endereco = db.Column(db.String(150), nullable=False)
+    telefone = db.Column(db.String(15), nullable=True)
+    cep = db.Column(db.String(9), nullable=True)
+    endereco = db.Column(db.String(150), nullable=True)
+    cidade = db.Column(db.String(50), nullable=True)
 
     def __repr__(self):
         return f"<Fornecedor {self.nome}>"
