48787147723d51f8580723472fdace946913e648
JulianoSaroba123 <julianosaroba19.jsp@gmail.com>
2025-06-20 15:06:38 -0300

    Update produto_model.py

--- models/produto_model.py
+++ models/produto_model.py
@@ -1,22 +1,22 @@
 from models import db
-from datetime import datetime
 
 class Produto(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    codigo = db.Column(db.String(20), unique=True)
     nome = db.Column(db.String(100), nullable=False)
-    codigo_barras = db.Column(db.String(50))
-    data = db.Column(db.Date, default=datetime.today)
-    fornecedor = db.Column(db.String(100))
-    unidade = db.Column(db.String(20))
-    classificacao = db.Column(db.String(50))
-    localizacao = db.Column(db.String(100))
-    situacao = db.Column(db.String(20))  # Ativo / Inativo
-    valor_venda = db.Column(db.Float)
-    valor_compra = db.Column(db.Float)
-    estoque = db.Column(db.Integer)
-    lucro = db.Column(db.Float)
-    fabricante = db.Column(db.String(100))
-    numero_serie = db.Column(db.String(100))
+    codigo_barras = db.Column(db.String(50), nullable=True)
+    data = db.Column(db.Date, nullable=True)
+    fornecedor = db.Column(db.String(100), nullable=True)
+    unidade = db.Column(db.String(20), nullable=True)
+    classificacao = db.Column(db.String(50), nullable=True)
+    localizacao = db.Column(db.String(100), nullable=True)
+    situacao = db.Column(db.String(20), default='Ativo')
+    valor_venda = db.Column(db.Float, nullable=True)
+    valor_compra = db.Column(db.Float, nullable=True)
+    estoque = db.Column(db.Integer, nullable=True)
+    lucro = db.Column(db.Float, nullable=True)
+    fabricante = db.Column(db.String(100), nullable=True)
+    numero_serie = db.Column(db.String(100), nullable=True)
 
     def __repr__(self):
         return f"<Produto {self.nome}>"
