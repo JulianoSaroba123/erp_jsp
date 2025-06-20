1d41972c248d7553eecf98843eb71859b4fa1b84
JulianoSaroba123 <julianosaroba19.jsp@gmail.com>
2025-06-20 15:05:44 -0300

    Update cliente_model.py

--- models/cliente_model.py
+++ models/cliente_model.py
@@ -2,13 +2,13 @@ from models import db
 
 class Cliente(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    codigo = db.Column(db.String(20))
-    nome = db.Column(db.String(100))
-    cpf_cnpj = db.Column(db.String(20))
-    telefone = db.Column(db.String(20))
-    email = db.Column(db.String(100))
-    endereco = db.Column(db.String(100))
-    numero = db.Column(db.String(10))
+    codigo = db.Column(db.String(20), unique=True)
+    nome = db.Column(db.String(100), nullable=False)
+    cpf_cnpj = db.Column(db.String(20), nullable=False)
+    telefone = db.Column(db.String(20), nullable=True)
+    email = db.Column(db.String(100), nullable=True)
+    endereco = db.Column(db.String(150), nullable=True)
+    numero = db.Column(db.String(10), nullable=True)
 
     def __repr__(self):
-        return f'<Cliente {self.nome}>'
+        return f"<Cliente {self.nome}>"
