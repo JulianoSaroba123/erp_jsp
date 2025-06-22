94c5ceb0a96d628c1dc7f8d28b7db6910cf6a008
JulianoSaroba123 <julianosaroba19.jsp@gmail.com>
2025-06-22 13:34:37 -0300

    Update tipo_servico_model.py

--- models/tipo_servico_model.py
+++ models/tipo_servico_model.py
@@ -1,1 +1,9 @@
+# models/tipo_servico_model.py
+from models import db
 
+class TipoServico(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"<TipoServico {self.nome}>"
