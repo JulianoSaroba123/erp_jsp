a27c8c4350e642de00319c8422037b42e930c055
JulianoSaroba123 <julianosaroba19.jsp@gmail.com>
2025-06-25 22:27:26 -0300

    Update tag_model.py

--- models/tag_model.py
+++ models/tag_model.py
@@ -1,1 +1,13 @@
+# models/tag_model.py
 
+from models import db
+
+class Tag(db.Model):
+    _tablename_ = 'tags'
+    id = db.Column(db.Integer, primary_key=True)
+    tag = db.Column(db.String(100), unique=True, nullable=False)       # Ex: {{cliente_nome}}
+    descricao = db.Column(db.String(255), nullable=True)               # Ex: "Nome do cliente"
+    modulo = db.Column(db.String(50), nullable=True)                   # Ex: "Cliente", "OS", etc.
+
+    def _repr_(self):
+        return f"<Tag {self.tag}>"
