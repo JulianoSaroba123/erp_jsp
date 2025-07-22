90232c4893bce1b36166204b2aa398fba8a620da
JulianoSaroba123 <julianosaroba19.jsp@gmail.com>
2025-07-22 15:00:41 -0300

    Delete models directory

--- models/db_config.py
+++
@@ -1,8 +0,0 @@
-# models/db_config.py
-
-# Responsável por criar a instância do SQLAlchemy
-# que será compartilhada entre os modelos e o app
-
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
